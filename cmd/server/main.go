6e90d8f417b504bf74e15a26fa0d32f469f1f4fd
erups <e.rupshis@gmail.com>
2023-10-08 14:46:38 +0200

    added controller

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/erupshis/zero_agency_test/internal/config"
+	"github.com/erupshis/zero_agency_test/internal/controller"
 	"github.com/erupshis/zero_agency_test/internal/logger"
 	"github.com/erupshis/zero_agency_test/internal/storage"
 	"github.com/erupshis/zero_agency_test/internal/storage/manager/ram"
@@ -21,8 +24,12 @@ func main() {
 	cfg := config.Parse()
 
 	ramManager := ram.Create(log)
-	mainStorage := storage.Create(log)
+	mainStorage := storage.Create(ramManager, log)
+	mainController := controller.Create(mainStorage, log)
 
-	log.Info(cfg.Host)
+	mainController.LaunchServer(cfg)
 
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, os.Interrupt, os.Kill, syscall.SIGTERM)
+	<-sigCh
 }
--- internal/controller/controller.go
+++ internal/controller/controller.go
@@ -1,11 +1,42 @@
 package controller
 
 import (
+	"github.com/erupshis/zero_agency_test/internal/config"
 	"github.com/erupshis/zero_agency_test/internal/logger"
+	"github.com/erupshis/zero_agency_test/internal/storage"
+	"github.com/gofiber/fiber/v2"
 )
 
 type Controller struct {
 	strg storage.BaseStorage
 
 	log logger.BaseLogger
+}
+
+func Create(baseStorage storage.BaseStorage, baseLogger logger.BaseLogger) *Controller {
+	return &Controller{
+		strg: baseStorage,
+		log:  baseLogger,
+	}
+}
+
+func (c *Controller) LaunchServer(cfg config.Config) {
+	go func(host string) {
+		app := fiber.New()
+		c.route(app)
+		c.log.Info("[Controller:LaunchServer] server is launching")
+		if err := app.Listen(host); err != nil {
+			c.log.Info("[Controller:LaunchServer] failed to launch server: %v", err)
+		}
+	}(cfg.Host)
+}
+
+func (c *Controller) route(app *fiber.App) {
+	app.Post("/edit/:Id", func(c *fiber.Ctx) error {
+		return c.SendString("Add")
+	})
+
+	app.Get("/list", func(c *fiber.Ctx) error {
+		return c.SendString("Read")
+	})
 }
